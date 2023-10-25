4c74c0d2bc633b260b88e21be55e91f83bb27ef9
timothearroyo <timothe.arroyo@4sh.fr>
2023-10-25 10:51:05 +0200

    spring initializr

---
+++ src/main/java/com/fsh/fmt/springJpaRest/core/WebApplication.java
@@ -0,0 +1,13 @@
+package com.fsh.fmt.springJpaRest.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsh/fmt/springJpaRest/core/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsh.fmt.springJpaRest.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
