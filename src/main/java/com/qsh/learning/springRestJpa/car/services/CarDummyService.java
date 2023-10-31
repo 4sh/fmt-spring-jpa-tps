0203a5b3a048ddb4a3a7c4b19035dc9a1145e586
timothearroyo <timothe.arroyo@4sh.fr>
2023-10-31 16:03:48 +0100

    tp2 step2 solution

--- src/main/java/com/qsh/learning/springRestJpa/car/services/CarDummyService.java
+++ src/main/java/com/qsh/learning/springRestJpa/car/services/CarDummyService.java
@@ -3,11 +3,11 @@ package com.qsh.learning.springRestJpa.car.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("prod")
+@Profile("dev")
 @Service
-public class CarService implements VehicleService {
+public class CarDummyService implements VehicleService {
 
     public String find() {
-        return "Car 1";
+        return "Dummy Car 1";
     }
 }
