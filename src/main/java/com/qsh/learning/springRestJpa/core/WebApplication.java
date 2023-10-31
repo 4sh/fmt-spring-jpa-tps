8ed3ec1ab1f8cc0c075b6c741293e7630de9346a
timothearroyo <timothe.arroyo@4sh.fr>
2023-10-31 16:54:25 +0100

    tp2 step 3 solution

--- src/main/java/com/qsh/learning/springRestJpa/core/WebApplication.java
+++ src/main/java/com/qsh/learning/springRestJpa/core/WebApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan("com.qsh")
+@PropertySource("classpath:config.properties")
 public class WebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
--- src/main/java/com/qsh/learning/springRestJpa/core/config/AppConfig.java
+++ src/main/java/com/qsh/learning/springRestJpa/core/config/AppConfig.java
@@ -1,12 +1,14 @@
 package com.qsh.learning.springRestJpa.core.config;
 
 import com.qsh.learning.springRestJpa.core.config.models.CurrentCarIdHolder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${user.id}")
     public String userId;
 
     @Bean
