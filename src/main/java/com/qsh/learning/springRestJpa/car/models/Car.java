1c9bc7702ab5c544ba1d86f6bbb046e920c15113
timothearroyo <timothe.arroyo@4sh.fr>
2023-11-07 19:50:49 +0100

    tp3 step3 solution

--- src/main/java/com/qsh/learning/springRestJpa/car/models/Car.java
+++ src/main/java/com/qsh/learning/springRestJpa/car/models/Car.java
@@ -1,14 +1,22 @@
 package com.qsh.learning.springRestJpa.car.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Car {
     private String id;
 
     private String brand;
 
+    @JsonIgnore
     private String model;
 
     private String color;
 
+    @JsonProperty("doors")
     private Integer numberOfDoors;
 
     private Integer numberOfSeats;
