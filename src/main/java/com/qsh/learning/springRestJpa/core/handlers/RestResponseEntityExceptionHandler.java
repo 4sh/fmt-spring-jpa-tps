c12406ca955bf585ac93bcafdc65ccb5b1d80805
timothearroyo <timothe.arroyo@4sh.fr>
2023-11-08 14:48:14 +0100

    add maven dependency + add exception handler for validation exception

--- src/main/java/com/qsh/learning/springRestJpa/core/handlers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/qsh/learning/springRestJpa/core/handlers/RestResponseEntityExceptionHandler.java
@@ -1,19 +1,40 @@
 package com.qsh.learning.springRestJpa.core.handlers;
 
 import com.qsh.learning.springRestJpa.car.exceptions.CarNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(value = {CarNotFoundException.class})
-	protected ResponseEntity<String> handleException(CarNotFoundException e) {
-		logger.error(e.getMessage(), e);
+    @ExceptionHandler(value = {CarNotFoundException.class})
+    protected ResponseEntity<String> handleException(CarNotFoundException e) {
+        logger.error(e.getMessage(), e);
 
-		return new ResponseEntity<>("Car not found", HttpStatus.NOT_FOUND);
-	}
+        return new ResponseEntity<>("Car not found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException e) {
+        logger.error(e.getMessage());
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    public final ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException e, HttpHeaders headers, HttpStatusCode status, WebRequest request
+    ) {
+        logger.error(e.getMessage());
+
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
