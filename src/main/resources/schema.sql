c847f53d14e8e4b55fcd661dd9120a06712c3a39
timothearroyo <timothe.arroyo@4sh.fr>
2023-11-11 17:20:57 +0100

    add scripts for license_plate and license_plate_id creation

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,3 +7,13 @@ CREATE TABLE IF NOT EXISTS car(
     number_of_doors int,
     number_of_seats int
     );
+
+CREATE TABLE IF NOT EXISTS license_plate(
+    id varchar(255) not null
+    primary key,
+    license_plate_number varchar(255) not null,
+    country varchar(255) not null
+);
+
+ALTER TABLE car ADD COLUMN IF NOT EXISTS license_plate_id varchar(255) constraint FK_region_id
+    references license_plate;
